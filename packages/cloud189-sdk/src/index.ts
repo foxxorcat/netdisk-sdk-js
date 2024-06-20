85449e5df44f7dd63dadb3491791222ae8c66df1
foxxorcat <foxxorcat@foxmail.com>
2024-06-21 00:17:08 +0800

    chore: 修订package.json

--- packages/cloud189-sdk/src/index.ts
+++ packages/cloud189-sdk/src/index.ts
@@ -5,6 +5,3 @@ export * from './sharefs_api'
 
 export * from './helper'
 export * from './errors'
-
-// @parcel/transformer-typescript-types 无法处理 tsconfig.json 配置
-import "../types"
--- packages/quarkUC-sdk/src/index.ts
+++ packages/quarkUC-sdk/src/index.ts
@@ -4,6 +4,3 @@ export * from './fs_api'
 export * from './const'
 export * from './errors'
 export * from './types'
-
-// @parcel/transformer-typescript-types 无法处理 tsconfig.json 配置
-import "../types"
